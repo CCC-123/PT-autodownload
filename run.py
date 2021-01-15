9c76820e090ea5ddb6f4bd4cae900079cc45e4e1
ccc <CCC-123@users.noreply.github.com>
2021-01-16 00:14:05 +0800

    1.catch error 2. modify code

--- run.py
+++ run.py
@@ -7,21 +7,23 @@ import shutil
 import transmissionrpc
 import time
 
+import yaml
+
 #settings
 #=========================================================
 mode = 'test'
 #mode = 'work'
 diskSize = 150 #GB
 
-
-username = ''
-password = ''
 #TODO:strategy
 
 searchCycle = 3600 #seconds
-downloadWaitTime = 30 #seconds
+downloadWaitTime = 60 #seconds
 
-
+downloadPath = os.path.join(os.getcwd(), 'download')
+history = []
+state = []
+freeSize = diskSize  
 #=========================================================
 def getLatestFileName(waitTime, path):
     # method to get the downloaded file name
@@ -43,6 +45,45 @@ def FIFO(source):
 
     state.append(source)
 
+def init():
+    req_url = "https://pt.sjtu.edu.cn"
+    chrome_options=Options()
+    if not mode == 'test':
+        chrome_options.add_argument('--headless')
+
+    if not os.path.exists(downloadPath):
+        os.makedirs(downloadPath)
+    prefs = {'profile.default_content_settings.popups': 0, 'download.default_directory':  downloadPath}
+    chrome_options.add_experimental_option('prefs', prefs)
+    browser = webdriver.Chrome(chrome_options=chrome_options)
+    browser.get(req_url)
+    return browser
+
+def login(browser):
+    if os.path.exists( os.path.join(os.getcwd(), 'info.yaml')) :
+        f = open( os.path.join(os.getcwd(), 'info.yaml'))
+        loginInfo = yaml.load(f) 
+        username = loginInfo['username']
+        password = loginInfo['password']
+    else:
+        username = raw_input("usernaem:")
+        password = raw_input('password:')
+    usernameInput = browser.find_element_by_name("username")
+    usernameInput.send_keys(username)
+    passwordInput = browser.find_element_by_name("password")
+    passwordInput.send_keys(password)
+    browser.find_element_by_xpath("//input[@value='登录']").click()
+
+def recordHistory(browser):
+    browser.find_element_by_partial_link_text('种').click()
+    freeSeedList = browser.find_elements_by_class_name('free_bg')
+    for freeSeed in freeSeedList:
+        seedName = freeSeed.find_element_by_xpath( "./td[2]/table/tbody/tr/td[1]/a").get_attribute('title')
+        history.append(seedName) 
+    if mode == 'test':
+        history.pop(0)
+        print(history)
+
 
 class Seed:
     def __init__(self, name):
@@ -53,69 +94,44 @@ class Seed:
         self.upload = 0
         self.download = 0
 
+# =========================================
 
-#登录
-req_url = "https://pt.sjtu.edu.cn"
-chrome_options=Options()
-if not mode == 'test':
-    chrome_options.add_argument('--headless')
-downloadPath = os.path.join(os.getcwd(), 'download')
-if not os.path.exists(downloadPath):
-    os.makedirs(downloadPath)
-prefs = {'profile.default_content_settings.popups': 0, 'download.default_directory':  downloadPath}
-chrome_options.add_experimental_option('prefs', prefs)
-browser = webdriver.Chrome(chrome_options=chrome_options)
-browser.get(req_url)
-usernameInput = browser.find_element_by_name("username")
-usernameInput.send_keys(username)
-passwordInput = browser.find_element_by_name("password")
-passwordInput.send_keys(password)
-browser.find_element_by_xpath("//input[@value='登录']").click()
-
-
-
-#记录首页种子，等待新资源
-browser.find_element_by_partial_link_text('种').click()
-freeSeedList = browser.find_elements_by_class_name('free_bg')
-history = []
-state = []
-for freeSeed in freeSeedList:
-    seedName = freeSeed.find_element_by_xpath( "./td[2]/table/tbody/tr/td[1]/a").get_attribute('title')
-    history.append(seedName) 
-if mode == 'test':
-    history.pop(0)
-    print(history)
+if __name__ == '__main__':
+    browser = init()
+    login(browser)
+    recordHistory(browser)
+    tc = transmissionrpc.Client(address='127.0.0.1', port=9091)
     
-#打开transmission
-tc = transmissionrpc.Client(address='127.0.0.1', port=9091)
-freeSize = diskSize  
 
-while True:
-    browser.refresh()
-    sourceList = browser.find_elements_by_class_name('free_bg')
-    for source in sourceList:
-        sourceName = source.find_element_by_xpath( "./td[2]/table/tbody/tr/td[1]/a").get_attribute('title')
-        if sourceName not in history:
-            #判定空间           
-            tmpSeed = Seed(sourceName)
-            size =  source.find_element_by_xpath( "./td[5]").text.split('\n')
-            print(size)
-            if size[1] == 'GB':
-                tmpSeed.size = float(size[0]) 
-            if size[1] == 'MB':
-                tmpSeed.size = float(size[0]) / 1000    
-            history.append(sourceName)
-            if(tmpSeed.size > diskSize):
-                continue  #放不下
-            FIFO(tmpSeed)
+    while True:
+        try:
+            browser.refresh()
+            sourceList = browser.find_elements_by_class_name('free_bg')
+            for source in sourceList:
+                sourceName = source.find_element_by_xpath( "./td[2]/table/tbody/tr/td[1]/a").get_attribute('title')
+                if sourceName not in history:
+                    #判定空间           
+                    tmpSeed = Seed(sourceName)
+                    size =  source.find_element_by_xpath( "./td[5]").text.split('\n')
+                    print(size)
+                    if size[1] == 'GB':
+                        tmpSeed.size = float(size[0]) 
+                    if size[1] == 'MB':
+                        tmpSeed.size = float(size[0]) / 1000    
+                    history.append(sourceName)
+                    if(tmpSeed.size > diskSize):
+                        continue  #放不下
+                    FIFO(tmpSeed)
 
-            #开始下载种子
-            print('Download \n', sourceName)
-            source.find_element_by_xpath( "./td[2]/table/tbody/tr/td[3]/a[1]").click()
-            cwd = os.getcwd()
-            latestFile = getLatestFileName(downloadWaitTime, downloadPath)  #wait time
-            tc.add_torrent(torrent = os.path.join(downloadPath, latestFile), download_dir = os.path.join(downloadPath, tmpSeed.savepath))
-    time.sleep(searchCycle)
+                    #开始下载种子
+                    print('Download \n', sourceName)
+                    source.find_element_by_xpath( "./td[2]/table/tbody/tr/td[3]/a[1]").click()
+                    cwd = os.getcwd()
+                    latestFile = getLatestFileName(downloadWaitTime, downloadPath)  #wait time
+                    tc.add_torrent(torrent = os.path.join(downloadPath, latestFile), download_dir = os.path.join(downloadPath, tmpSeed.savepath))
+        except Exception as e:
+            print('Error:', e)
+        time.sleep(searchCycle)
 
-browser.close()
-browser.quit()
+    browser.close()
+    browser.quit()
